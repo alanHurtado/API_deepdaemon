881760c181ccb9ab377bd09b6b30bbcaff557e30
alanHurtado <alanasto10@gmail.com>
2022-03-09 14:54:15 -0600

    conexion a BD Cpanel

--- src/database.js
+++ src/database.js
@@ -10,10 +10,10 @@ const mysql = require('mysql');
 /*BD EN HOSTING*/
 
 const mysqlConnection = mysql.createConnection({
-    host : 'us-cdbr-east-05.cleardb.net',
-    user : 'b63b479c2bd710',
-    password : '1a256d73',
-    database : 'heroku_00e8da110b6cd01'
+    host : 'deepdaemon.org',
+    user : 'deepdae1_admin',
+    password : 'D33pD43monC1C2022',
+    database : 'deepdae1_adminweb'
     
 });
 
